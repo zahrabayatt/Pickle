9206ed3f12e71b3124d4a302eb8a0693d05dcff9
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-07-29 23:27:12 +0330

    Fetching result form on the client from twilio server app

--- index.js
+++ index.js
@@ -9,23 +9,8 @@ const app = new Vue({
   },
   methods: {
     async loadImages() {
-      // TODO: Use the Messaging API to use submitted photos
-      // const response = await fetch("/api/")
-      // TODO: Create a web-based API that matches this expected response
-      this.gallery = [
-        {
-          src: "https://picsum.photos/id/82/200/300",
-          description: "Look at these flowers",
-          alt: "some flowers",
-          thumbnailWidth: "200px",
-        },
-        {
-          src: "https://picsum.photos/id/19/300/300",
-          description: "A tree",
-          alt: "It is beautiful:)",
-          thumbnailWidth: "200px",
-        },
-      ];
+      const response = await fetch("https://pickle-1039.twil.io/api/pics");
+      this.gallery = await response.json();
     },
   },
   mounted() {
